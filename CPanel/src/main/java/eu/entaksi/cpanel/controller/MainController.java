1826942c0fe741b37c82a0238f5140b8b0d48f92
Domenico Conti <dm.conti@gmail.com>
2015-08-24 16:37:07 +0200

    esempio di modifica di un testo presente nella Scene al click su Button

--- CPanel/src/main/java/eu/entaksi/cpanel/controller/MainController.java
+++ CPanel/src/main/java/eu/entaksi/cpanel/controller/MainController.java
@@ -1,7 +1,19 @@
 package eu.entaksi.cpanel.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * Created by uzumaki on 24/08/15.
  */
 public class MainController {
+
+    @FXML
+    public TextField myTextField;
+
+    @FXML
+    protected void doSomething() {
+        myTextField.clear();
+        myTextField.setText("The button was clicked!");
+    }
 }
